7dbccb17cae5b6dc879136ba0d393a0f61ea5fda
Fırat KÜÇÜK <firatkucuk@gmail.com>
2017-08-23 12:22:33 +0200

    initial import

---
+++ src/main/java/io/vapp/web/VappWebApplication.java
@@ -0,0 +1,13 @@
+package io.vapp.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VappWebApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(VappWebApplication.class, args);
+    }
+}
