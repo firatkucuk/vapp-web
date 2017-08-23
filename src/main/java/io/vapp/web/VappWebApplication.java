092272a2ebe5ba67e9689480d577a1c5434410b8
Fırat KÜÇÜK <firatkucuk@gmail.com>
2017-08-23 13:11:23 +0200

    checkstyle fixes

--- src/main/java/io/vapp/web/VappWebApplication.java
+++ src/main/java/io/vapp/web/VappWebApplication.java
@@ -3,10 +3,11 @@ package io.vapp.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 @SpringBootApplication
 public class VappWebApplication {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
 
         SpringApplication.run(VappWebApplication.class, args);
     }
